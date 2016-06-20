df06385566d21271fe38e9713bc66f75c0e19f50
cypherix93 <bikram.ghosh93@gmail.com>
2016-06-19 20:15:01 -0400

    Improved the states.

--- src/private/angular/states/auth/Login.js
+++ src/private/angular/states/auth/Login.js
@@ -1,35 +1,38 @@
-AngularApp.config(function($stateProvider)
+AngularApp.config(function ($stateProvider)
 {
     $stateProvider.state("login",
         {
             url: "/login",
             templateUrl: "views/auth/login.html",
-            controller: function ($scope, $state, AuthSvc, IdentitySvc, ModalSvc, toastr)
-            {
-                $scope.login = function ()
-                {
-                    if (!$scope.email || !$scope.password)
-                    {
-                        toastr.error("Both email and password needs to be provided.");
-                        return;
-                    }
-
-                    AuthSvc.loginUser($scope.email, $scope.password)
-                        .then(function (response)
-                        {
-                            if (!response.success)
-                            {
-                                toastr.error(response.message);
-                                return;
-                            }
-
-                            // Redirect to home page
-                            $state.go("home");
-
-                            // Display toast message
-                            toastr.success("Welcome back " + IdentitySvc.currentUser.email);
-                        });
-                };
-            }
+            controller: "LoginController as Login"
         });
 });
+AngularApp.controller("LoginController", function LoginController($scope, $state, AuthSvc, IdentitySvc, ModalSvc, toastr)
+{
+    var self = this;
+    
+    self.login = function ()
+    {
+        if (!self.email || !self.password)
+        {
+            toastr.error("Both email and password needs to be provided.");
+            return;
+        }
+        
+        AuthSvc.loginUser(self.email, self.password)
+            .then(function (response)
+            {
+                if (!response.success)
+                {
+                    toastr.error(response.message);
+                    return;
+                }
+                
+                // Redirect to home page
+                $state.go("home");
+                
+                // Display toast message
+                toastr.success("Welcome back " + IdentitySvc.currentUser.email);
+            });
+    };
+})
--- src/private/angular/states/auth/Register.js
+++ src/private/angular/states/auth/Register.js
@@ -4,34 +4,37 @@ AngularApp.config(function ($stateProvider)
         {
             url: "/register",
             templateUrl: "views/auth/register.html",
-            controller: function ($scope, $state, AuthSvc, IdentitySvc, ModalSvc, toastr)
-            {
-                $scope.user = {};
-
-                $scope.register = function ()
-                {
-                    if (!$scope.user.email || !$scope.user.password)
-                    {
-                        toastr.error("Both email and password needs to be provided.");
-                        return;
-                    }
-
-                    AuthSvc.registerUser($scope.user)
-                        .then(function (response)
-                        {
-                            if (!response.success)
-                            {
-                                toastr.error(response.message);
-                                return;
-                            }
-
-                            // Redirect to login
-                            $state.go("login");
-
-                            // Display toast message
-                            toastr.success("Thanks for signing up " + $scope.user.email);
-                        });
-                };
-            }
+            controller: "RegisterController as Register"
         });
 });
+AngularApp.controller("RegisterController", function RegisterController($scope, $state, AuthSvc, IdentitySvc, ModalSvc, toastr)
+{
+    var self = this;
+
+    self.user = {};
+
+    self.register = function ()
+    {
+        if (!self.user.email || !self.user.password)
+        {
+            toastr.error("Both email and password needs to be provided.");
+            return;
+        }
+
+        AuthSvc.registerUser(self.user)
+            .then(function (response)
+            {
+                if (!response.success)
+                {
+                    toastr.error(response.message);
+                    return;
+                }
+
+                // Redirect to login
+                $state.go("login");
+
+                // Display toast message
+                toastr.success("Thanks for signing up " + self.user.email);
+            });
+    };
+})
